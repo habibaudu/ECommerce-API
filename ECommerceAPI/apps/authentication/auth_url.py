4d7b94e94eba7353af76ac63e4af7f85bec378d8
Habib Audu <auduhabib1990@gmail.com>
2019-07-16 10:20:17 +0100

    Revert "Authentication"

--- ECommerceAPI/apps/authentication/auth_url.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from ECommerceAPI.apps.authentication import views
-
-
-urlpatterns = [
-    path("auth/login/", views.LoginView.as_view(), name='login'),
-    path("auth/register/", views.RegisterView.as_view(), name='register'),
-]
--- ECommerceAPI/apps/department/department_url.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from ECommerceAPI.apps.department import views
-
-urlpatterns = [
-    path("departments/", views.DepartmentView.as_view()),
-    path("department/<int:id>/", views.DepartmentDetailView.as_view())
-]
